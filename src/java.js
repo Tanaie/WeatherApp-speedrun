a4fb77b4dac778ffb1af2f83efe7cc0de9a45a6f
Tanaie <87162695+Tanaie@users.noreply.github.com>
2021-08-31 23:39:16 -0400

    Updated the time and some other shit

--- src/java.js
+++ src/java.js
@@ -1,3 +1,22 @@
+function formatDate(timestamp){
+// calculate the date
+let date = new Date(timestamp);
+let hours = date.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = date.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let day = days[date.getDay()];
+return `${day} ${hours}:${minutes}`
+
+
+}
+
+
 function displayTemp(response) {
   let temperatureElement = document.querySelector("#tempy");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
@@ -10,6 +29,8 @@ function displayTemp(response) {
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
   console.log(response.data.wind.speed);
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
