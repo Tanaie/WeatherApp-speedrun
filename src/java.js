f6de7bf8f95eeb25bf2c0917d0087f2a90d02c25
Tanaie <87162695+Tanaie@users.noreply.github.com>
2021-08-31 23:19:42 -0400

    Updating eveerrything!
    Creating everything... :)

---
+++ src/java.js
@@ -0,0 +1,18 @@
+function displayTemp(response) {
+  let temperatureElement = document.querySelector("#tempy");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  console.log(response.data.wind.speed);
+}
+
+let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Toronto&units=metric&appid=${apiKey}`;
+
+axios.get(apiUrl).then(displayTemp);
