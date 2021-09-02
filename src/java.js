c07521956165f47aff851bc279bd488efb20c854
Tanaie <87162695+Tanaie@users.noreply.github.com>
2021-09-02 00:39:13 -0400

    Added a fahrenheight and celsius conversion

--- src/java.js
+++ src/java.js
@@ -1,21 +1,26 @@
-function formatDate(timestamp){
-// calculate the date
-let date = new Date(timestamp);
-let hours = date.getHours();
-if (hours < 10) {
-  hours = `0${hours}`;
+function formatDate(timestamp) {
+  // calculate the date
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
-let minutes = date.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-}
-let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-let day = days[date.getDay()];
-return `${day} ${hours}:${minutes}`
-
-
-}
-
 
 function displayTemp(response) {
   let temperatureElement = document.querySelector("#tempy");
@@ -32,14 +37,19 @@ function displayTemp(response) {
   let dateElement = document.querySelector("#date");
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
   let iconElement = document.querySelector("#icon");
-  iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  celsiusTemp = Math.round(response.data.main.temp);
 }
 
 function search(city) {
-let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
-axios.get(apiUrl).then(displayTemp);
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(displayTemp);
 }
 
 function handleSubmit(event) {
@@ -48,10 +58,33 @@ function handleSubmit(event) {
   search(cityInputElement.value);
 }
 
+function displayFahrenTemp(event) {
+  event.preventDefault();
+  let fahrenheitTemp = Math.round((celsiusTemp * 9) / 5 + 32);
+  celsiusLink.classList.remove("active");
+  fahrenLink.classList.add("active");
+  let tempElement = document.querySelector("#tempy");
+  tempElement.innerHTML = fahrenheitTemp;
+}
+
+function displaycelsiusTemp(event) {
+  event.preventDefault();
+  let tempElement = document.querySelector("#tempy");
+  celsiusLink.classList.add("active");
+  fahrenLink.classList.remove("active");
+  tempElement.innerHTML = celsiusTemp;
+}
+
+let celsiusTemp = null;
 
 search("Toronto");
-
 
 let form = document.querySelector("#search-form");
 
 form.addEventListener("submit", handleSubmit);
+
+let fahrenLink = document.querySelector("#fahren-link");
+fahrenLink.addEventListener("click", displayFahrenTemp);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displaycelsiusTemp);
